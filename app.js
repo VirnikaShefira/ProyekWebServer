5cc08524786f57e8031c04ebf73d68181fc6a2f3
VirnikaShefira <virnikashefira@gmail.com>
2023-04-01 22:32:06 +0700

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Virnika Shefira Gina Morissa\n')
 })
 
 app.listen(PORT, HOST)
